f0f38525a77ba86d4a10ad9a335cfa39b479e443
Salvinus01 <86074085+Salvinus01@users.noreply.github.com>
2021-06-29 12:58:26 -0300

    Initial commit

---
+++ app/src/main/java/br/com/oceanbrasil/ocean_android_push_notification/MainActivity.kt
@@ -0,0 +1,27 @@
+package br.com.oceanbrasil.ocean_android_push_notification
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.messaging.FirebaseMessaging
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().subscribeToTopic("weather")
+            .addOnCompleteListener { task ->
+                var msg = getString(R.string.msg_subscribed)
+                if (!task.isSuccessful) {
+                    msg = getString(R.string.msg_subscribe_failed)
+                }
+                Log.d(TAG, msg)
+                Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+            }
+
+    }
+}
